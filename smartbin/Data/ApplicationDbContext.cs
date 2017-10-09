f030f7cfc348f76eb24e15d11cf57dfd36461548
DESKTOP-KBAS7Q8\Ruvinda Ranaweera <rkruvinda@gmail.com>
2017-10-09 14:38:29 +0530

    dailyinput_tbl

--- smartbin/Data/ApplicationDbContext.cs
+++ smartbin/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace smartbin.Data
         public DbSet<Feedback> feedback { get; set; }
         public DbSet<Workforce> workforce_tbl { get; set; }
         public DbSet<working> working_tbl { get; set; }
+        public DbSet<dailyinput> dailyinput_tbl { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ smartbin/Data/Models/dailyinput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smartbin.Data.Models
+{
+    public class dailyinput
+    {
+        [Key]
+        public string binID { get; set; }
+        public string areaID { get; set; }
+        public double PA { get; set; } = 0;
+        public double PL { get; set; } = 0;
+        public double GL { get; set; } = 0;
+        public double BD { get; set; } = 0;
+    }
+}
