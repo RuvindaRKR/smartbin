a5126a47635b2bf0f31500c1a5d23da521ea0338
DESKTOP-3GTCASK\dabar <dabarera.shehan27@gmail.com>
2017-10-09 15:31:32 +0530

    brenda

---
+++ smartbin/Data/Migrations/20171009082150_wen_binLevel_tbl.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace smartbin.Data.Migrations
+{
+    public partial class wen_binLevel_tbl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "wen_binLevel_tbl",
+                columns: table => new
+                {
+                    binID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    week_1_0930 = table.Column<double>(type: "float", nullable: false),
+                    week_1_1530 = table.Column<double>(type: "float", nullable: false),
+                    week_1_2130 = table.Column<double>(type: "float", nullable: false),
+                    week_2_0930 = table.Column<double>(type: "float", nullable: false),
+                    week_2_1530 = table.Column<double>(type: "float", nullable: false),
+                    week_2_2130 = table.Column<double>(type: "float", nullable: false),
+                    week_3_0930 = table.Column<double>(type: "float", nullable: false),
+                    week_3_1530 = table.Column<double>(type: "float", nullable: false),
+                    week_3_2130 = table.Column<double>(type: "float", nullable: false),
+                    week_4_0930 = table.Column<double>(type: "float", nullable: false),
+                    week_4_1530 = table.Column<double>(type: "float", nullable: false),
+                    week_4_2130 = table.Column<double>(type: "float", nullable: false),
+                    week_5_0930 = table.Column<double>(type: "float", nullable: false),
+                    week_5_1530 = table.Column<double>(type: "float", nullable: false),
+                    week_5_2130 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_wen_binLevel_tbl", x => x.binID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "wen_binLevel_tbl");
+        }
+    }
+}
